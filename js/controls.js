e4617ab30a58e3e2e80b98aefb263d3dafec5930
Marcelo Melo <marcellomello90@gmail.com>
2023-01-27 23:38:18 +0100

    #	modified:   index.html #	modified:   js/controls.js #	modified:   js/elements.js #	new file:   js/events.js #	modified:   js/index.js #	modified:   js/sounds.js #	modified:   js/timer.js #	new file:   sounds/audio_buttonPress.wav #	new file:   sounds/audios_kichenTimer.mp3 #	new file:   sounds/coffee.wav #	new file:   sounds/ferePlace.wav #	new file:   sounds/forest.wav #	new file:   sounds/rain.wav #	modified:   style.css

---
+++ js/controls.js
@@ -0,0 +1,48 @@
+//   buttonPlay,
+//   buttonPause,
+//   buttonStop,
+//   buttonUp,
+//   buttonDown,
+
+export default function Controls({
+  buttonPause,
+  buttonPlay,
+  // buttonSet,
+  buttonStop,
+}) {
+
+  function play() {
+    buttonPlay.classList.add('hide')
+    buttonPause.classList.remove('hide')
+    // buttonSet.classList.add('hide')
+    // buttonStop.classList.remove('hide')
+  }
+
+  function pause() {
+    buttonPause.classList.add('hide')
+    buttonPlay.classList.remove('hide')
+  }
+  
+  function stop() {
+    buttonPlay.classList.remove('hide')
+    buttonPause.classList.add('hide')
+    // buttonSet.classList.remove('hide')
+    buttonStop.classList.add('hide')
+  }
+
+  function getMinutes() {
+    let newMinutes = prompt('Quantos minutos?')
+    if (!newMinutes) {
+      return false
+    }
+
+    return newMinutes
+  }
+
+  return {
+    // reset,
+    play,
+    pause,
+    getMinutes
+  }
+}
